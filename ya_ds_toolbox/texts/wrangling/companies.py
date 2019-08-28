366547e3846e2f6e92d3fe27cd190caefc315210
OctoMiao <lei.ma@homelike.cc>
2019-08-28 15:42:57 +0200

    improve company name cleaner

--- ya_ds_toolbox/texts/wrangling/companies.py
+++ ya_ds_toolbox/texts/wrangling/companies.py
@@ -7,7 +7,12 @@ logger = logging.getLogger('texts.wrangling.companies')
 
 
 def clean_company_name(
-    company_name, remove_chars=None, remove_legal_form=None
+    company_name,
+    remove_chars=None,
+    chars_data=None,
+    remove_legal_form=None,
+    legal_form_data=None,
+    company_name_map=None
     ):
     """Clean company name by removing unnecessary strings.
 
@@ -22,11 +27,21 @@ def clean_company_name(
             )
             )
     logger.debug(f'clean_company_name:: company_name is {company_name}')
-    if remove_legal_form is None:
-        remove_legal_form = _ELF_CODE
 
+    if legal_form_data:
+        remove_legal_form = True
+    if remove_legal_form is None:
+        remove_legal_form = False
+
+    if legal_form_data and (legal_form_data is None):
+        legal_form_data = _ELF_CODE
+
+    if chars_data:
+        remove_chars = True
     if remove_chars is None:
-        remove_chars = [
+        remove_chars = False
+    if remove_chars and (chars_data is None):
+        chars_data = [
             '"', "'", '(', ')', '[', ']'
             ]
 
@@ -36,32 +51,34 @@ def clean_company_name(
 
     company_name = company_name.strip()
 
-    logger.debug(f'clean_company_name:: legalform for {company_name}')
-    for elf in remove_legal_form:
-        spaced_elf = ' ' + elf
-        if company_name.endswith(spaced_elf):
-            logger.debug(f'Removing {elf} from company name {company_name}')
-            company_name = company_name[:-len(spaced_elf)]
+    if remove_legal_form:
+        logger.debug(f'clean_company_name:: legalform for {company_name}')
+        for elf in legal_form_data:
+            spaced_elf = ' ' + elf
+            if company_name.endswith(spaced_elf):
+                logger.debug(f'Removing {elf} from company name {company_name}')
+                company_name = company_name[:-len(spaced_elf)]
 
-    company_name = company_name.strip()
+        company_name = company_name.strip()
 
-    # remove special characters from company name
-    for char in remove_chars:
-        company_name = company_name.replace(char, '')
+    if remove_chars:
+        # remove special characters from company name
+        for char in chars_data:
+            company_name = company_name.replace(char, '')
 
     # convert to lower case
     company_name = company_name.lower()
 
     logger.debug(f'clean_company_name:: company_name_enforcement for {company_name}')
     # Rename companies using a pre-defined map
-    company_name_map = {}
-    logger.debug(
-        'company_data::looked through company name enforcement: {}'.format(
-            company_name_map
-            )
-        )
     if company_name_map:
-        company_name = company_name_map.get('company')
-        logger.debug(f'company_data::company_name_map exists: {company_name}')
+        logger.debug(
+            'company_data::looked through company name enforcement: {}'.format(
+                company_name_map
+                )
+            )
+        if company_name_map:
+            company_name = company_name_map.get('company')
+            logger.debug(f'company_data::company_name_map exists: {company_name}')
 
     return company_name
